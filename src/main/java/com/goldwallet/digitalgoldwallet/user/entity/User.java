f47825e94879d28dc9c734c425d920b0701f1eff
Pavithra <pavithravigneshwaran29@gmail.com>
2026-04-13 15:52:29 +0530

    Added user module

---
+++ src/main/java/com/goldwallet/digitalgoldwallet/user/entity/User.java
@@ -0,0 +1,28 @@
+package com.goldwallet.digitalgoldwallet.user.entity;
+
+import com.goldwallet.digitalgoldwallet.user.entity.Address;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String name;
+
+    private Double balance;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+}
