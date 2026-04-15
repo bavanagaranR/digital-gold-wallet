88360d508c0e7dab75298d43e6e3a272e4039511
subhaa-3 <subhapaari0306@gmail.com>
2026-04-15 14:06:28 +0530

    modified Gold Repsoitories

--- src/main/java/com/goldwallet/digitalgoldwallet/gold/entity/VirtualGoldHolding.java
+++ src/main/java/com/goldwallet/digitalgoldwallet/gold/entity/VirtualGoldHolding.java
@@ -1,4 +1,40 @@
 package com.goldwallet.digitalgoldwallet.gold.entity;
 
+import com.goldwallet.digitalgoldwallet.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "virtual_gold_holdings")
+@Getter
+@Setter
+
 public class VirtualGoldHolding {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "holding_id")
+    private Long holdingId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "branch_id", nullable = false)
+    private VendorBranch branch;
+
+    @Column(nullable = false, precision = 18, scale = 2)
+    private BigDecimal quantity;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
--- src/main/java/com/goldwallet/digitalgoldwallet/gold/repository/PhysicalGoldRepository.java
+++ src/main/java/com/goldwallet/digitalgoldwallet/gold/repository/PhysicalGoldRepository.java
@@ -1,7 +1,7 @@
 package com.goldwallet.digitalgoldwallet.gold.repository;
 
+import com.goldwallet.digitalgoldwallet.gold.entity.PhysicalGoldTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhysicalGoldRepository extends JpaRepository<PhysicalGoldTransaction, Long> {
 }
-
