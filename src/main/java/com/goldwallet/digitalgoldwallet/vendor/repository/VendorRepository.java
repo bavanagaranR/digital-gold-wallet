5da931bf274f74741bcf7d0f376182f08d085ae2
CaitlynMary <156457013+CaitlynMary@users.noreply.github.com>
2026-04-15 11:56:50 +0530

    Vendor (#18)
    * added vendor entity
    
    * add ventor entity and repository

---
+++ src/main/java/com/goldwallet/digitalgoldwallet/vendor/repository/VendorRepository.java
@@ -0,0 +1,7 @@
+package com.goldwallet.digitalgoldwallet.vendor.repository;
+
+import com.goldwallet.digitalgoldwallet.vendor.entity.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendorRepository extends JpaRepository<Vendor,Long> {
+}
