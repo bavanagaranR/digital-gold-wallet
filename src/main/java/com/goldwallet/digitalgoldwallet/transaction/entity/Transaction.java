d4fe47b051e12432c0f32d72c2bb0bb3bff350be
Mirudhula <160239987+Mirudhula-06@users.noreply.github.com>
2026-04-15 14:19:43 +0530

    Transaction (#19)
    * added Transaction entity
    
    * added  transaction repository
    
    * added repository interface
    
    * modified Transaction repository
    
    ---------
    
    Co-authored-by: bavanagaranR <163574985+bavanagaranR@users.noreply.github.com>

--- src/main/java/com/goldwallet/digitalgoldwallet/transaction/entity/Transaction.java
+++ src/main/java/com/goldwallet/digitalgoldwallet/transaction/entity/Transaction.java
@@ -1,36 +1,59 @@
 package com.goldwallet.digitalgoldwallet.transaction.entity;
 
-import com.goldwallet.digitalgoldwallet.user.entity.User;
+import com.project.digitalgoldwallet.user.entity.User;
+import com.project.digitalgoldwallet.vendor.entity.VendorBranch;
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
 @Table(name = "transaction_history")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TransactionHistory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer transactionId;
-
-    private String transactionType;
-    // Example: "Buy", "Sell", "Convert to Physical"
-
-    private String transactionStatus;
-    // Example: "Success", "Failed"
-
-    private Double quantity;
-    private Double amount;
-
-    private LocalDateTime createdAt;
+    @Column(name = "transaction_id")
+    private Long transactionId;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @NotNull
     private User user;
 
     @ManyToOne
     @JoinColumn(name = "branch_id")
+    @NotNull
     private VendorBranch branch;
+
+    // ENUM replaced with String
+    @NotBlank
+    @Column(name = "transaction_type")
+    private String transactionType;
+
+    @NotBlank
+    @Column(name = "transaction_status")
+    private String transactionStatus;
+
+    @NotNull
+    private BigDecimal quantity;
+
+    @NotNull
+    private BigDecimal amount;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
+
