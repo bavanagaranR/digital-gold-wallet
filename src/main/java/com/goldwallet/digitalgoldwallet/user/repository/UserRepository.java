dd5869a80f9ec12ba2da7e7261f6bc070c6af6f4
Pavithra <pavithravigneshwaran29@gmail.com>
2026-04-15 11:01:34 +0530

    updated user repository and entity

--- src/main/java/com/goldwallet/digitalgoldwallet/user/repository/UserRepository.java
+++ src/main/java/com/goldwallet/digitalgoldwallet/user/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.goldwallet.digitalgoldwallet.user.repository;
 
+
+
+
 import com.goldwallet.digitalgoldwallet.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
-
 }
