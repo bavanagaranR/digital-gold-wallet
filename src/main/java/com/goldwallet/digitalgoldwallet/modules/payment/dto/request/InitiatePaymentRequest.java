9f903c1f02fabfb583d6699ac801f968963c94a5
bavanagaranR <bavanab42@gmail.com>
2026-04-17 10:55:16 +0530

    completed payment , wallet custom query & entity modification

--- src/main/java/com/goldwallet/digitalgoldwallet/modules/payment/dto/request/InitiatePaymentRequest.java
+++ src/main/java/com/goldwallet/digitalgoldwallet/modules/payment/dto/request/InitiatePaymentRequest.java
@@ -7,7 +7,7 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 
-@Data
+
 public class InitiatePaymentRequest {
     @NotNull(message = "User ID is required")
     private Long userId;
@@ -21,4 +21,36 @@ public class InitiatePaymentRequest {
 
     @NotNull(message = "Transaction type is required")
     private Payment.TransactionType transactionType;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Payment.PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(Payment.PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Payment.TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(Payment.TransactionType transactionType) {
+        this.transactionType = transactionType;
+    }
 }
