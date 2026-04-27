9bdcaf2af1ec660c7e88e98a550c53941943f74e
bavanagaranR <163574985+bavanagaranR@users.noreply.github.com>
2026-04-27 15:27:49 +0530

    validation added to frondend (#90)
    * added comment lines for wallet and payment modules
    
    * added comment lines
    
    * frontend validation added
    
    
    
    Co-authored-by: Mirudhula <160239987+Mirudhula-06@users.noreply.github.com>

--- frontend/src/app/features/wallet/pages/wallet-debit/wallet-debit.component.ts
+++ frontend/src/app/features/wallet/pages/wallet-debit/wallet-debit.component.ts
@@ -28,7 +28,13 @@ export class WalletDebitComponent {
   loading = false;
 
   submit() {
-    if (!this.userId) {
+    
+     if(parseInt(this.userId) <= 0)
+    {
+      this.error = 'User ID must be a greater than 0';
+      return;
+    }
+    else if (!this.userId) {
       this.error = 'User ID is required';
       return;
     }
